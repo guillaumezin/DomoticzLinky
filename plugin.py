34f2c84f18a8601b426dd0038b93fe6ec6a4f9d5
Guillaume Zin <guillaume.zin@gmail.com>
2020-08-25 13:35:02 +0200

    Correct end of tariff 2, change version to 2.0.0

--- plugin.py
+++ plugin.py
@@ -21,7 +21,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 """
-<plugin key="linky" name="Linky" author="Barberousse" version="2.0.0-beta-3" externallink="https://github.com/guillaumezin/DomoticzLinky">
+<plugin key="linky" name="Linky" author="Barberousse" version="2.0.0" externallink="https://github.com/guillaumezin/DomoticzLinky">
     <params>
         <param field="Mode4" label="Heures creuses (vide pour désactiver, cf. readme pour la syntaxe)" width="500px" required="false" default="">
 <!--        <param field="Mode4" label="Heures creuses" width="500px">
@@ -626,7 +626,7 @@ class BasePlugin:
             return False
         
         for lDateInterval in lHc:
-            if (tDate >= lDateInterval[0]) and (tDate <= lDateInterval[1]):
+            if (tDate >= lDateInterval[0]) and (tDate < lDateInterval[1]):
                 return True
         return False
 
